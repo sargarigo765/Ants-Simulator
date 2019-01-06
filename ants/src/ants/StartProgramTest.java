80d5db132b3398067dba0ea1a3d0509d0ae60baf
Gyarmati Krisztián <steel765@gmail.com>
2019-01-06 13:59:34 +0100

    Vannak most már tesztek is, minden demo teszt zöld.

---
+++ ants/src/ants/StartProgramTest.java
@@ -0,0 +1,16 @@
+package ants;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StartProgramTest
+{
+
+	@Test
+	void testMain()
+	{
+		fail("Not yet implemented");
+	}
+
+}
