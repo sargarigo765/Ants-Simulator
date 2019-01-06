a47e9d84c32d91c5ab9302090b77d7f73b19324b
Gyarmati Krisztián <steel765@gmail.com>
2019-01-06 13:59:58 +0100

    Új fájlok.

--- ants/src/ants/StartProgramTest.java
+++ ants/src/ants/StartProgramTest.java
@@ -1,16 +1,66 @@
 package ants;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class StartProgramTest
 {
+	protected static int value1;
+	protected static int value2;
+	String message = "Hello World";
+	MessageUtil messageUtil = new MessageUtil(message);
 
-	@Test
-	void testMain()
+	// assigning the values
+	@BeforeAll
+	public static void setUp()
 	{
-		fail("Not yet implemented");
+		value1 = 3;
+		value2 = 3;
+		System.out.println("JUnit test run: setUp, value1=" + value1 + ", value2=" + value2);
 	}
 
+	@Test
+	void testJUintCorrectWorking()
+	{
+		// Ellenőrzés: két sztring egyenlő-e?
+		String str = "Junit is working fine";
+		assertEquals("Junit is working fine", str);
+	}
+
+	@Test
+	public void testAddForTwoInitialisedValues()
+	{
+		// Ellenőrzés: az eredmény igaz?
+		double result = value1 + value2;
+		System.out.println("JUnit test run: testAddForTwoInitialisedValues, value1=" + value1 + ", value2=" + value2
+				+ ", result=" + result);
+		assertTrue(result == 6);
+	}
+
+	@Test
+	public void testPrintMessage()
+	{
+		System.out.println("Inside testPrintMessage()");
+		assertEquals(message, messageUtil.printMessage());
+	}
+
+	@Test
+	public void testArrayElements()
+	{
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(42);
+		list.add(-3);
+		list.add(17);
+		list.add(99);
+
+		assertEquals(4, list.size());
+		assertTrue(list.contains(-3));
+		assertFalse(list.isEmpty());
+	}
 }
